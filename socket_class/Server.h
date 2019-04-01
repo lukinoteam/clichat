088d804f1f236050a727d30b6bd66c5fa7c5080d
dungdl28d11 <33054551+dungdl28d11@users.noreply.github.com>
2019-04-01 14:35:31 +0700

    files for Socket connection
    - Class and Header file for Server and Client
    - Enums for ActiveStatus

---
+++ socket_class/Server.h
@@ -0,0 +1,27 @@
+#ifndef SERVER_H
+#define SERVER_H
+#include <winsock2.h>
+#include <string>
+using namespace std;
+class Server
+{
+  private:
+    //MARK:- Connection data
+    string roomId;
+    WSADATA WSAData;
+    SOCKET serverSocket;
+    SOCKADDR_IN serverAddr;
+    void setRoomId();
+    //MARK:- Singleton object
+    static Server *instance;
+    //MARK:- private constructor to prevent outside initiation
+    Server();
+
+  public:
+    //MARK:- access data
+    static Server * getInstance();
+    string getRoomId();
+    void start();
+    void run();
+};
+#endif
