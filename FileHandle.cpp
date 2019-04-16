9391d2a21bdae7e954dcfbcd6d2f588ad849a795
xwind1999 <40222758+xwind1999@users.noreply.github.com>
2019-04-16 23:05:26 +0700

    Upload more functions

--- FileHandle.cpp
+++ FileHandle.cpp
@@ -20,6 +20,50 @@ void FileHandle::addData(string content, string file) {
 		myfile.close();
 	}
 }
+void FileHandle::replace(string content,string file_content,string file_replace) {
+	string word = "";
+	ifstream fileContent;
+	ifstream fileReplace;
+	istringstream ss(content);
+	string new_string = "";
+	while (ss >> word) {
+		fileContent.open(file_content);
+		string temp = "";
+		int line = 0;
+		while (getline(fileContent, temp)) {
+			line++;
+			if (temp == word) {
+				fileReplace.open(file_replace);
+				string replace_string = "";
+				for (int i = 1; i <= line; i++) {
+					getline(fileReplace, replace_string);
+				}
+				word = replace_string;
+				fileReplace.close();
+				break;
+			}
+		}
+		fileContent.close();
+		new_string += word + " ";
+		//delete the last space
+	 }
+	int length = new_string.size() - 1;
+	new_string = new_string.substr(0, length);
+	cout << new_string;
+
+} 
+void FileHandle::addFilterData(string content, string replace, string file_content, string file_replace) {
+	ofstream fileContent;
+	ofstream fileReplace;
+	fileContent.open(file_content, ios::app);
+	fileReplace.open(file_replace, ios::app);
+	if (fileContent.is_open() && fileReplace.is_open()) {
+		fileContent << content << endl;
+		fileReplace << replace << endl;
+		fileContent.close();
+		fileReplace.close();
+	}
+}
 void FileHandle::showData(string file) {
 	ifstream myfile;
 	myfile.open(file);
@@ -37,12 +81,15 @@ int FileHandle::isExistData(string content, string file) {
 	string line;
 	while (getline(myfile, line)) {
 		if (line == content) {
+			myfile.close();
 			return 1;
 		}
 	}
+	myfile.close();
 	return -1;
 }
-void FileHandle::removeData(string content, string file) {
+//temp_file is directory of temp file
+void FileHandle::removeData(string content, string file,char temp_file[100]) {
 	//read a file
 	ifstream readFile;
 	//write into a temp file(just rewrite list without content lines)
@@ -50,7 +97,7 @@ void FileHandle::removeData(string content, string file) {
 	//directory of list
 	readFile.open(file);
 	//directory of temp list
-	tempFile.open("");
+	tempFile.open(temp_file);
 	if (readFile.is_open()) {
 		string line = "";
 		while (getline(readFile, line)) {
@@ -63,7 +110,7 @@ void FileHandle::removeData(string content, string file) {
 	}
 	//rewrite the list and remove temp list
 	//temp file directory
-	readFile.open("");
+	readFile.open(temp_file);
 	tempFile.open(file);
 	if (readFile.is_open()) {
 		string line = "";
@@ -74,6 +121,6 @@ void FileHandle::removeData(string content, string file) {
 		readFile.close();
 	}
 	//remove the tempFile
-	remove("");
+	remove(temp_file);
 }
 
--- FileHandle.h
+++ FileHandle.h
@@ -2,14 +2,19 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 using namespace std;
 class FileHandle
 {
 public:
 	FileHandle();
 	~FileHandle();
+	//ham test , voi string content se hien ra mot string moi voi noi dung thay doi
+	void replace(string content,string file_content,string file_replace);
+	//add data cho filter
+	void addFilterData(string content, string replace, string file_content,string file_replace);
 	void addData(string content,string file);
-	void removeData(string content,string file);
+	void removeData(string content,string file,char temp_file[100] );
 	void showData(string file);
 	int isExistData(string content, string file);
 };
